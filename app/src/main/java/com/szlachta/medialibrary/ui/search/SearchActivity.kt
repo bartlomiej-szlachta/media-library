0a2e8ec630b0e2ad5aeab50228d257ff06218e2d
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2020-04-10 19:51:09 +0200

    feat: search activity toolbar

--- app/src/main/java/com/szlachta/medialibrary/ui/search/SearchActivity.kt
+++ app/src/main/java/com/szlachta/medialibrary/ui/search/SearchActivity.kt
@@ -3,11 +3,20 @@ package com.szlachta.medialibrary.ui.search
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.szlachta.medialibrary.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        setSupportActionBar(action_bar_search)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
