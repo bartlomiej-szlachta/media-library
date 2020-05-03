a9b8ccab0e29476be15dcc5400eafea01dbe4548
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2020-05-03 10:07:53 +0200

    feat: display raw firebase data

--- app/src/main/java/com/szlachta/medialibrary/ui/home/TabItemFragment.kt
+++ app/src/main/java/com/szlachta/medialibrary/ui/home/TabItemFragment.kt
@@ -4,20 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.szlachta.medialibrary.R
 import com.szlachta.medialibrary.ui.ItemStatusEnum
 import com.szlachta.medialibrary.ui.ItemTypeEnum
+import kotlinx.android.synthetic.main.fragment_tab_item.text_view
 
 class TabItemFragment(
     private val itemType: ItemTypeEnum,
     private val itemStatus: ItemStatusEnum
 ) : Fragment() {
 
+    private val valueEventListener = object : ValueEventListener {
+        override fun onDataChange(p0: DataSnapshot) {
+            text_view.text = p0.value.toString()
+        }
+
+        override fun onCancelled(p0: DatabaseError) {
+            Toast.makeText(activity, p0.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_tab_item, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Firebase.database.reference.child(itemType.key).addValueEventListener(valueEventListener)
     }
 }
