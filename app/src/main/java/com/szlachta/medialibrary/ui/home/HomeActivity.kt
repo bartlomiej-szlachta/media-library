b5926b3f689f4d371440dda6acb9770ccb50209d
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2020-05-13 12:48:10 +0200

    fix: display proper action bar title on application resume

--- app/src/main/java/com/szlachta/medialibrary/ui/home/HomeActivity.kt
+++ app/src/main/java/com/szlachta/medialibrary/ui/home/HomeActivity.kt
@@ -40,17 +40,26 @@ class HomeActivity : AppCompatActivity() {
     private val floatingActionButtonOnClickListener = View.OnClickListener {
         val intent = Intent(this, FormActivity::class.java)
             .putExtra(FormModeEnum.TAG, FormModeEnum.CREATE)
-                // TODO: pass item status to the form (to select the right chip)
+            // TODO: pass item status to the form (to select the right chip)
             .putExtra(ItemTypeEnum.TAG, getItemType())
         startActivity(intent)
     }
 
-    private val navigationItemOnClickListener =
+    private val navigationItemSelectedListener =
         BottomNavigationView.OnNavigationItemSelectedListener {
             if (bottom_navigation.selectedItemId == it.itemId) {
-                return@OnNavigationItemSelectedListener false
+                false
+            } else {
+                val position = when (it.itemId) {
+                    R.id.action_games -> ItemTypeEnum.GAMES.position
+                    R.id.action_movies -> ItemTypeEnum.MOVIES.position
+                    R.id.action_books -> ItemTypeEnum.BOOKS.position
+                    else -> ItemTypeEnum.GAMES.position
+                }
+                pager_home.setCurrentItem(position, false)
+                setActionBarTitle()
+                true
             }
-            handleBottomTabSelection(it.itemId)
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,7 +75,13 @@ class HomeActivity : AppCompatActivity() {
 
         action_bar_home.setOnClickListener(actionBarOnClickListener)
         floating_action_button_add.setOnClickListener(floatingActionButtonOnClickListener)
-        bottom_navigation.setOnNavigationItemSelectedListener(navigationItemOnClickListener)
+        bottom_navigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
+    }
+
+    // fixes a bug; TODO: fix the bug other way
+    override fun onResume() {
+        super.onResume()
+        setActionBarTitle()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -91,25 +106,6 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
-    private fun handleBottomTabSelection(actionId: Int): Boolean = when (actionId) {
-        R.id.action_games -> {
-            action_bar_home_title.text = getString(R.string.search_games)
-            pager_home.setCurrentItem(ItemTypeEnum.GAMES.position, false)
-            true
-        }
-        R.id.action_movies -> {
-            action_bar_home_title.text = getString(R.string.search_movies)
-            pager_home.setCurrentItem(ItemTypeEnum.MOVIES.position, false)
-            true
-        }
-        R.id.action_books -> {
-            action_bar_home_title.text = getString(R.string.search_books)
-            pager_home.setCurrentItem(ItemTypeEnum.BOOKS.position, false)
-            true
-        }
-        else -> false
-    }
-
     private fun signOut() {
         val intent = SignInActivity.getLaunchIntent(this)
         startActivity(intent)
@@ -118,4 +114,13 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun getItemType(): ItemTypeEnum = ItemTypeEnum.getByPosition(pager_home.currentItem)
+
+    private fun setActionBarTitle() {
+        action_bar_home_title.text = when (pager_home.currentItem) {
+            ItemTypeEnum.GAMES.position -> getString(R.string.search_games)
+            ItemTypeEnum.MOVIES.position -> getString(R.string.search_movies)
+            ItemTypeEnum.BOOKS.position -> getString(R.string.search_books)
+            else -> ""
+        }
+    }
 }
